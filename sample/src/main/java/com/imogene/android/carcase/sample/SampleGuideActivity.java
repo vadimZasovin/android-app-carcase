35b23a79619e2ffcf4528af85f8d2e8629d28ecd
vadim <vzasovin@gmail.com>
2017-06-08 10:53:23 +0300

    removed BaseGuideActivity class

--- sample/src/main/java/com/imogene/android/carcase/sample/SampleGuideActivity.java
+++ sample/src/main/java/com/imogene/android/carcase/sample/SampleGuideActivity.java
@@ -1,16 +1,18 @@
 package com.imogene.android.carcase.sample;
 
-import com.imogene.android.carcase.controller.BaseGuideActivity;
+import android.content.Intent;
+
+import com.imogene.android.carcase.controller.BaseActivity;
 import com.imogene.android.carcase.controller.BaseGuideFragment;
 
 /**
  * Created by Admin on 06.06.2017.
  */
 
-public class SampleGuideActivity extends BaseGuideActivity {
+public class SampleGuideActivity extends BaseActivity {
 
     @Override
-    protected BaseGuideFragment createGuideFragment() {
+    protected BaseGuideFragment createFragment(Intent intent) {
         return SampleGuideFragment.newInstance();
     }
 }
